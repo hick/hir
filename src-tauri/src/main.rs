aaeddf005a5f2fe0666a4cc0c6400ee7511ed7f3
Hick <hickwu@qq.com>
2024-01-27 03:57:30 +0800

    disable close button in rust
    disable close button in src-tauri/src/main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,6 +9,11 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            window.set_decorations(false).expect("Failed to set window decorations");
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
