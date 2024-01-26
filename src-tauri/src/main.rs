bba1d37971fdc815c4af9ca8fd238db36fac9685
Hick <hickwu@qq.com>
2024-01-27 04:36:34 +0800

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
+use tauri::{Manager, GlobalShortcutManager};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -13,7 +13,20 @@ fn main() {
     tauri::Builder::default()
         .setup(|app| {
             let window = app.get_window("main").unwrap();
+            // 设置不显示关闭按钮等
             window.set_decorations(false).expect("Failed to set window decorations");
+            // 240127-041259 注册全局快捷键
+            app.global_shortcut_manager()
+                .register("Ctrl+Alt+6", move || {
+                    let is_visible = window.is_visible().unwrap();
+                    if is_visible {
+                        window.hide().unwrap();
+                    } else {
+                        window.show().unwrap();
+                        window.set_focus().unwrap();
+                    }
+                })
+                .expect("Failed to register global shortcut");
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![greet])
